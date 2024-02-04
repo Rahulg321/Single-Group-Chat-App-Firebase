45f6b90220fe6d332daa7c64e4c980ddbf7a53d8
Rahul Gupta <rg5353070@gmail.com>
2024-02-04 18:31:44 +0530

    fixed some design changes

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -24,38 +24,40 @@ const ChatRoom = () => {
   console.log("empty messages are ", messages);
 
   return (
-    <div className="bg-zinc-900">
+    <div className=" bg-zinc-900">
       {/* <div className="background-gradient absolute inset-0 z-0  max-h-screen"></div> */}
       <div className="pointer-events-none absolute inset-0 z-10 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       <div className="narrow-container">
         <div className="mx-auto flex-col border">
-          <header className="sticky top-0 flex justify-between border-b-4 bg-gray-900 px-4 py-6">
+          <header className="sticky top-0 flex justify-between border-b-4 bg-zinc-900 px-4 py-6">
             <span className="text-3xl font-bold text-white">Free Talk ❤️‍🔥</span>
             <SignoutButton />
           </header>
-          <div className=" bg-gray-900">
+          <div className="min-h-[80vh] bg-slate-900">
             {loading && (
-              <div className="flex min-h-screen flex-col items-center justify-start">
+              <div className="flex flex-col items-center justify-start">
                 <span className="text-white">Loading Messages..........</span>
               </div>
             )}
-            {messages && !messages.empty ? (
-              messages.docs.map((msg) => {
-                const msgData = msg.data() as TChatMessage;
-                return (
-                  <ChatMessage
-                    text={msgData.text}
-                    key={msg.id}
-                    photoUrl={msgData.photoUrl}
-                    uid={msgData.uid}
-                  />
-                );
-              })
-            ) : (
-              <div className="flex min-h-screen flex-col items-center justify-start">
-                <span className="text-white">No new Messages</span>
-              </div>
-            )}
+            <div className="">
+              {messages && !messages.empty ? (
+                messages.docs.map((msg) => {
+                  const msgData = msg.data() as TChatMessage;
+                  return (
+                    <ChatMessage
+                      text={msgData.text}
+                      key={msg.id}
+                      photoUrl={msgData.photoUrl}
+                      uid={msgData.uid}
+                    />
+                  );
+                })
+              ) : (
+                <div className="flex flex-col items-center justify-start">
+                  <span className="text-white">No new Messages</span>
+                </div>
+              )}
+            </div>
             <div ref={scrollRef}></div>
           </div>
 
